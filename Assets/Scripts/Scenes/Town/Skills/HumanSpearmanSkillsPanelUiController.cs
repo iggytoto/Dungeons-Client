b35184cff5b37120566e1f8dc8dfafc7566f2294
iggytoto <iggytoto@gmail.com>
2023-01-25 20:42:45 +0300

    phoenix raid description

--- Assets/Scripts/Scenes/Town/Skills/HumanSpearmanSkillsPanelUiController.cs
+++ Assets/Scripts/Scenes/Town/Skills/HumanSpearmanSkillsPanelUiController.cs
@@ -7,16 +7,8 @@ namespace DefaultNamespace.Ui.Scenes.Town.Skills
 {
     public class HumanSpearmanSkillsPanelUiController : SkillPanelUiControllerBase
     {
-        [SerializeField] private Button doubleEdgeL1;
-        [SerializeField] private Button doubleEdgeL2;
-        [SerializeField] private Button doubleEdgeL3;
-        [SerializeField] private Button doubleEdgeL4;
-        [SerializeField] private Button doubleEdgeL5;
-        [SerializeField] private Button midRangeL1;
-        [SerializeField] private Button midRangeL2;
-        [SerializeField] private Button midRangeL3;
-        [SerializeField] private Button midRangeL4;
-        [SerializeField] private Button midRangeL5;
+        [SerializeField] private Button doubleEdge;
+        [SerializeField] private Button midRange;
 
         private void OnTrainMidRangeClicked()
         {
@@ -32,31 +24,15 @@ namespace DefaultNamespace.Ui.Scenes.Town.Skills
 
         protected override void SetupButtons()
         {
-            doubleEdgeL1.onClick.AddListener(OnTrainDoubleEdgeClicked);
-            doubleEdgeL2.onClick.AddListener(OnTrainDoubleEdgeClicked);
-            doubleEdgeL3.onClick.AddListener(OnTrainDoubleEdgeClicked);
-            doubleEdgeL4.onClick.AddListener(OnTrainDoubleEdgeClicked);
-            doubleEdgeL5.onClick.AddListener(OnTrainDoubleEdgeClicked);
-            midRangeL1.onClick.AddListener(OnTrainMidRangeClicked);
-            midRangeL2.onClick.AddListener(OnTrainMidRangeClicked);
-            midRangeL3.onClick.AddListener(OnTrainMidRangeClicked);
-            midRangeL4.onClick.AddListener(OnTrainMidRangeClicked);
-            midRangeL5.onClick.AddListener(OnTrainMidRangeClicked);
+            doubleEdge.onClick.AddListener(OnTrainDoubleEdgeClicked);
+            midRange.onClick.AddListener(OnTrainMidRangeClicked);
         }
 
         protected override void ProcessButtons()
         {
             var skills = GetSkills<HumanSpearmanSkills>();
-            doubleEdgeL1.enabled = skills.doubleEdgePoints == 0;
-            doubleEdgeL2.enabled = skills.doubleEdgePoints == 1;
-            doubleEdgeL3.enabled = skills.doubleEdgePoints == 2;
-            doubleEdgeL4.enabled = skills.doubleEdgePoints == 3;
-            doubleEdgeL5.enabled = skills.doubleEdgePoints == 4;
-            midRangeL1.enabled = skills.midRangePoints == 0;
-            midRangeL2.enabled = skills.midRangePoints == 1;
-            midRangeL3.enabled = skills.midRangePoints == 2;
-            midRangeL4.enabled = skills.midRangePoints == 3;
-            midRangeL5.enabled = skills.midRangePoints == 4;
+            doubleEdge.enabled = skills.doubleEdgePoints + skills.midRangePoints < 6;
+            midRange.enabled = skills.doubleEdgePoints + skills.midRangePoints < 6;
         }
     }
 }
